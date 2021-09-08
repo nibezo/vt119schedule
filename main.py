8d10673715d7f0dd79344e929038c8f26556d335
nibezo <nibezo.cs@gmail.com>
2021-09-09 00:30:48 +0300

    feat: add DEBUG

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import datetime
+import logging
 from vkwave.bots import (SimpleLongPollBot)
 from vktoken import token
 bot = SimpleLongPollBot(tokens=token, group_id=207028531)
+logging.basicConfig(level="DEBUG")
 
 # status of the week, if 0 - Знаменатель, if 1 - Числитель
 dt = datetime.datetime.today()
