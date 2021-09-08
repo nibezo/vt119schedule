663e2d7c3600f1b575a5cf460ec30dd573ad95ef
nibezo <nibezo.cs@gmail.com>
2021-09-09 00:27:14 +0300

    feat: add vkwave

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import datetime
+from vkwave.bots import (SimpleLongPollBot)
+from vktoken import token
+bot = SimpleLongPollBot(tokens=token, group_id=207028531)
 
 # status of the week, if 0 - Знаменатель, if 1 - Числитель
 dt = datetime.datetime.today()
 status = datetime.date(dt.year, dt.month, dt.day).isocalendar()[1] % 2
+
+bot.run_forever()
